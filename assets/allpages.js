9741467c903af2a8c3701aa12f7c9bc5d6b65fb2
Julian Ranieri <jumpingafterrain@gmail.com>
2020-11-19 14:32:00 -0800

    demo is about ready

---
+++ assets/allpages.js
@@ -0,0 +1,16 @@
+//hamburger
+function hamburger() {
+    var button = document.getElementById("container");
+    var content = document.getElementById("container-content");
+    var whole = document.getElementById("whole");
+    button.classList.toggle("change")
+    if(content.style.display !== 'flex') {
+      content.style.display = 'flex';
+    //   whole.style.opacity = 0.1;
+    }
+    else {
+      content.style.display = 'none';
+    //   whole.style.opacity = 1;
+    }
+  }
+//end hamburger
---
+++ assets/frontpage.js
@@ -0,0 +1,30 @@
+var header = document.getElementById('header')
+header.style.display = 'none'
+header.style.animationName = 'header'
+//scroll functions
+window.onscroll = () => {
+    scrollFunction()
+  }
+  function scrollFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var header = document.getElementById('header')
+    var slant = document.getElementById('slant')
+
+    if (winScroll > 10) {
+        header.style.display = 'flex'
+        slant.style.display = 'block'
+    } else {
+        header.style.display = 'none'
+        slant.style.display = 'none'
+    }
+  
+    // if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    //     document.getElementById("first-section").className = "slideUp";
+    // }
+    // if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+    //     document.getElementById("second-section").className = "slideUp2";
+    // }
+    // if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 1000) {
+    //     document.getElementById("projects-div").className = "slideUp3";
+    // }
+  }
